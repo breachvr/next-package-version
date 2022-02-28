83416c2eda958a1c1847cbd85df69004fd96615a
Weibye <13300393+Weibye@users.noreply.github.com>
2022-02-28 14:03:21 +0100

    Making updates

--- src/Action.ts
+++ src/Action.ts
@@ -1,28 +1,76 @@
 import * as core from '@actions/core';
+import { ReleaseType } from 'semver';
 import simpleGit, { SimpleGit } from 'simple-git'
+import { Semver } from './Semver';
 
-class Action {
+const default_tag: string = '0.1.0';
+
+export class Action {
 
     private git: SimpleGit;
+    private semver: Semver;
 
     constructor() {
         this.git = simpleGit();
+        this.semver = new Semver();
     }
 
     public async Run() {
-        const release = core.getInput('release');
-        const pre_release = core.getBooleanInput('pre-release');
 
-        let response = await this.git.tags();
+        let release = this.GetReleaseType();
+        let pre_release = this.GetPreRelease();
 
-        response.all.forEach(element => {
-            console.log('Tag: ' + element);
-        });
+        if (!release || pre_release === undefined)
+        {
+            // throw new Error('Invalid inputs');
+        }
+        else
+        {
+            let next_tag: string;
+
+            let git_tags = await this.git.tags();
+
+            // If we find some tags
+            if (git_tags.all && git_tags.all.length > 0) {
+                let tags = this.semver.ValidTags(git_tags.all);
+                let latest = this.semver.GetLatest(tags);
+
+                next_tag = this.semver.GetNextVersion(latest, release, pre_release);
+                console.log('Next tag: ' + next_tag);
+            } else {
+                next_tag = default_tag;
+                console.log('No tags found in repo, using default: ' + next_tag);
+            }
+
+            core.setOutput('version', next_tag);
+        }
+    }
+
+    private GetReleaseType() : ReleaseType | undefined {
+        try {
+            const input = core.getInput('release');
+            if (!input) {
+                throw Error("Invalid release mode! Valid inputs are 'major', 'minor' and 'patch'");
+            }
+            return input as ReleaseType;
+        } catch(e) {
+            console.log("Error: " + (e as Error).message);
+        }
+    }
+
+    private GetPreRelease() : boolean | undefined {
+        try {
+            const result = core.getBooleanInput('pre-release');
+            return result;
+        } catch(e) {
+            console.log("Error: " + (e as Error).message);
+        }
     }
 }
 
 try {
     new Action().Run();
 } catch (error: any) {
+    console.log('Action failed: ' + (error as Error).message);
     core.setFailed(error.message);
 }
